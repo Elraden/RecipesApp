23674c4bd2b25cd196486645891a74b7795df02e
Elraden <morozovsa94@yandex.ru>
2025-03-07 15:01:33 +0300

    RA-04-created fragment and added it to main activity

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.recipesapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentTransaction
 import com.example.recipesapp.databinding.ActivityMainBinding
 
 
@@ -16,11 +16,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fmCategoryContainer, CategoriesListFragment())
+        transaction.commit()
     }
 }
