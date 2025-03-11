44f5f154819be31cef39751e22cd83f0de1c435a
Elraden <morozovsa94@yandex.ru>
2025-03-11 20:41:52 +0300

    RA-06-generated entities for local data base

--- app/src/main/java/com/example/recipesapp/FavoritesFragment.kt
+++ app/src/main/java/com/example/recipesapp/FavoritesFragment.kt
@@ -8,20 +8,11 @@ import androidx.fragment.app.Fragment
 import com.example.recipesapp.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
-
-    private var _binding: FragmentFavoritesBinding? = null
-    private val binding
-        get() = _binding ?: throw IllegalStateException("binding must not be null")
+    private val binding by lazy { FragmentFavoritesBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
